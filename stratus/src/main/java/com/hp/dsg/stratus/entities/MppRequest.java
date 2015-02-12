5e71ca0c0b56395883ed440c303f76e8fb30b4ff
pe-pan <pe.pan@centrum.cz>
2015-02-12 14:52:48 +0000

    BUG: when serializing a field (marked with the "field_" prefix), do not serialize the "field_" prefix into field name

--- stratus/src/main/java/com/hp/dsg/stratus/entities/MppRequest.java
+++ stratus/src/main/java/com/hp/dsg/stratus/entities/MppRequest.java
@@ -17,7 +17,7 @@ public class MppRequest extends CsaEntity {
         StringBuilder b = new StringBuilder();
         for (String key : properties.keySet()) {
             Object value = properties.get(key);
-            if (key.startsWith("field")) {
+            if (key.startsWith("field_")) {
                 fields.put(key, value);
             } else {
                 b.append("\"").append(key).append("\" : ").append(toJsonString(value)).append(", ");
@@ -30,7 +30,7 @@ public class MppRequest extends CsaEntity {
             b.append(", \"fields\" : {");
             for (String key : fields.keySet()) {
                 Object value = fields.get(key);
-                b.append("\"").append(key).append("\" : ").append(toJsonString(value)).append(", ");
+                b.append("\"").append(key.substring("field_".length())).append("\" : ").append(toJsonString(value)).append(", ");
             }
             b.deleteCharAt(b.length()-1);  // remove the very last comma
             b.deleteCharAt(b.length()-1);
