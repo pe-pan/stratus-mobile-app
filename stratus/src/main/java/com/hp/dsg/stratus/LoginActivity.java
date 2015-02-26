543c6259cde4e83bca9646ab625158884c65bf72
pe-pan <pe.pan@centrum.cz>
2015-02-26 15:18:17 +0000

    REF: cleaning

--- stratus/src/main/java/com/hp/dsg/stratus/LoginActivity.java
+++ stratus/src/main/java/com/hp/dsg/stratus/LoginActivity.java
@@ -186,7 +186,6 @@ public class LoginActivity extends StratusActivity {
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
             if ("sdk_google_phone_x86".equals( Build.PRODUCT )) {
                 System.setProperty("java.net.useSystemProxies", "false");
                 System.setProperty("https.proxyHost", "proxy.bbn.hp.com");
